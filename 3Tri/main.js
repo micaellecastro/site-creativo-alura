328abaf97f07a10bd9d212b02f7be04f146a8d7a
micaellecastro <128195744+micaellecastro@users.noreply.github.com>
2023-11-11 10:54:00 -0300

    Add files via upload

---
+++ 3Tri/main.js
@@ -0,0 +1,39 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+   if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+//alert('Elemento não encontrado');
+console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+   }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador< listaDeTeclas.lenght; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]; //template string
+const idAudio = '#som_${instrumento}';
+
+tecla[contador].onclick = function () {
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento) {
+
+   if (evento.code === 'Space' or evento.code === 'Enter') {
+tecla.classList.add('ativa'); 
+  }
+
+  }
+
+tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+}
+
+}
